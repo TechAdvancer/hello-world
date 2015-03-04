040defed57bd6f48836713c3b5907683199db0c1
Tech_Advancer <TechAdvancer@gmail.com>
2015-03-04 05:10:02 -0600

    Placeholder Code
    Just some code to get the basic idea started

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Hello World</title>
+<link href="./style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+<!-- contains the page title and short description -->
+<div id="header">
+<h1>Hello World</h1>
+</div> <!-- end header -->
+
+<div id="content"> <!-- content container below the header -->
+<!-- random links are random, for now. This is just a placeholder until the real stuff is made. -->
+
+<!-- div to hold links on the left page -->
+<div class="leftPage">
+<!-- later, use php or javascript to auto-insert these links -->
+<h3>Work:</h3>
+<a href="https://www.youtube.com/channel/UC7Oms5WI9khbAYFynExPNjQ">TheMorceMan's Youtube Channel</a><br>
+<a href="http://www.breakblock.org/">Break Block</a><br> <!-- Update this to https when ssl cert is up -->
+...<br><br>
+<a href="#"><< Previous Page</a><!-- use php or javascript to automate this -->
+</div> <!-- end leftPage -->
+
+<!-- div to hold links on the right page -->
+<div class="rightPage">
+<!-- later, use php or javascript to auto-insert these links -->
+<h3>Fun &amp; Games</h3>
+<a href="http://www.newgrounds.com/">Newgrounds</a><br>
+<a href="http://www.planetside-universe.com/character-5428011263311592001.php">Tech's Planetside 2 stats</a><br>
+...<br><br>
+<a href="#">Next Page >></a><!-- use php or javascript to automate this -->
+</div> <!-- end rightPage -->
+
+</div> <!-- end content -->
+
+</body>
+</html>
